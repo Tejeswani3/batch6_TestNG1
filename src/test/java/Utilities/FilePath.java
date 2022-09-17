3c7d7136a7b32dcf31df147e6be88d742fa973fc
GADDAM TEJESWANI <tejagaddam2000@gmail.com>
2022-09-17 11:58:05 +0530

    "first commit"

--- src/test/java/Utilities/FilePath.java
+++ src/test/java/Utilities/FilePath.java
@@ -1,5 +1,6 @@
 package Utilities;
 
 public class FilePath {
-
+	public static final String propertiesPath = "C:\\Users\\Reddy's\\eclipse-workspace\\TESTNGDEMO2\\src\\test\\resources\\config.properties";
+	public static final String XLPath = "C:\\Users\\Reddy's\\eclipse-workspace\\TESTNGDEMO2\\ExcelFiles\\TestXL.xlsx";
 }
